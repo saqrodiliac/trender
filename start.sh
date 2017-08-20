90b2cfc7d3dec6618b6d3c5a0173f9497662e759
Saqib Ali <saqib@cisco.com>
2017-08-20 10:47:02 -0400

    Built start.sh script and template for Flask

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#
+# bootstrap the virtual env if necessary
+#
+if [ -f "v/bin/activate" ]
+then
+    echo virtual env already created
+else
+    virtualenv -p python3 v
+fi
+source v/bin/activate
+
+# Just install dependencies by default to pick up any changes
+pip install -r requirements.txt
+
+#
+# run the data server
+#
+cd app
+python app.py
