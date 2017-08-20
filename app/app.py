524f2a599e0e10c0be9aef947997dbbaf45f9132
Saqib Ali <saqib@cisco.com>
2017-08-20 11:14:35 -0400

    Added bootstrap

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
 def hello():
-    return 'Hello Hayden!'
+    return render_template('index.html')
 
 app.run(host='0.0.0.0', port=8100, debug=True)
